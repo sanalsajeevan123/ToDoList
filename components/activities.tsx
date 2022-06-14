7ee10223bb8285fd0453ab22201b43bf15892633
sanalsajeevan123 <sanalsajeevan123@gmail.com>
2022-06-14 22:17:30 +0530

    14-6-to-do update with react icons

--- components/activities.tsx
+++ components/activities.tsx
@@ -1,14 +1,22 @@
-const Activities = () => {
+import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+type MyProps = {
+    slNo:string,
+    content:string
+}
+
+const Activities = (props:MyProps) => {
     return(
         <div className="flex items-center justify-between h-10 border">
             <div className="w-16 h-full bg-transparent flex items-center justify-center">
-                No.
+                {props.slNo}
             </div>
-            <div className="w-full h-full bg-transparent flex items-center">
-                Activity
+            <div className="w-full h-full bg-transparent flex items-center font-semibold">
+                {props.content}
             </div>
-            <div className="w-16 h-full bg-gray-200 flex items-center justify-center font-bold text-2xl">
-                -
+            <div className={`w-16 h-full bg-gray-200 flex items-center justify-center font-bold text-md hover:bg-gradient-to-tl hover:from-red-500 hover:to-black hover:text-white`}>
+                <FontAwesomeIcon icon={faTimes}/>
             </div>
         </div>
     )
